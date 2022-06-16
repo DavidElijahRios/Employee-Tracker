219fc3fa5e2db4290f714f13893a190c7a892942
David Rios <david.riostech@outlook.com>
2022-06-15 17:57:56 -0700

    finished mapping out application just need to finish writing a few more functions and readme.md file and application will be complete

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const init = () => {
     {
       type: 'list',
       message: "What would you like to do?",
-      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update employee role'],
       name: "userChoice"
     },
   ])
@@ -54,58 +54,185 @@ const init = () => {
         init();
       })
     } else if (answers.userChoice === 'Add a department') {
+       addDeptQ();
+
+    } else if (answers.userChoice === 'Add a role') {
+      addRoleQ();
+
+    } else if (answers.userChoice === 'Add an employee') {
+      addEmployeeQ();
+
+    } else if (answers.userChoice === 'Update employee role') {
+      updateEmployeeQ();
 
     }
   })
 }
 
-// TODO:Left off --- finish up!!
+
+
+
 const addDeptQ = () => {
    inquirer.prompt([
     {
        type: "input",
-       message: "what is the name of the new department",
-       title: "newDepartment"
+       message: "what is the name of the new department?",
+       name: "newDepartment"
     },
 
   ])
   .then((answers) => {
-    db.query(`${answers.newDepartment}`, function (err) {
+    db.query(`
+    INSERT INTO department (name)
+       VALUES 
+          ("${answers.newDepartment}")`, function (err) {
       if (err) throw err;
-      console.log('New Department Added!')
+      console.log('New Department Added!');
+      init();
     })
   })
 }
 
-// TODO: make a function to call when add a role is selected
+
+// ============================================================================
+// ============================================================================
+// Functions to access new updated data from database
+
+// Function to get the department from db
+const deptArr = [];
+
+const updatedDeptDb = () => {
+  db.query("SELECT * from department", function (err, res) {
+    if (err) throw err;
+    for (let i = 0; i < res.length; i++) {
+      deptArr.push(res[i].name)
+    }
+ })
+ return deptArr;
+
+} 
+
+// TODO: Function to get Roles from db
+
+
+
+
+// TODO: Function to get Manager options from db
+
+
+
+
+// TODO: Function to get all employees in a list from db
+
+
+
+
+// TODO: Function to get all roles in a list from db
+
+
+
+
+
+
+
+// ============================================================================
+// ============================================================================
+// Functions to use in application
+
+
 const addRoleQ = () => {
-  return inquirer.prompt([
+  
+  
+ return inquirer.prompt([
     {
-
+      type: "input",
+      message: "what is the name of the new Role?",
+      name: "newRole"
     },
-
+    {
+      type: "input",
+      message: "what is the salary of the Role?",
+      name: "newRoleSal"
+    },
+    {
+      type: "list",
+      message: "Which department does the Role belong to?",
+      choices: updatedDeptDb(),
+      name: "newRolDept"
+    },
   ])
+
+  .then((answers) => {
+    const roleId = updatedDeptDb().indexOf(answers.newRolDept) + 1
+     db.query(`
+     INSERT INTO roles (title, salary, department_id)
+       VALUES
+            ("${answers.newRole}", ${answers.newRoleSal}, ${roleId})
+       `, function (err) {
+         if (err) throw err;
+         console.log('New Role Added!')
+         init();
+       })
+  })
 }
 
 
-// TODO: make a function to call when add a department is selected
+
+// TODO: add created function to call back for app functionality
+// !DO NOT FORGET to call back init() at the end of these functions below
+
 const addEmployeeQ = () => {
   return inquirer.prompt([
     {
-
+      type: "input",
+      message: "What is the first name of the new Employee?",
+      name: "firstName"
+    },
+    {
+      type: "input",
+      message: "What is the last name of the new Employee?",
+      name: "lastName"
+    },
+    {
+      type: "list",
+      message: "What is the Employee's Role?",
+      choices: [],
+      name: "empRole"
+    },
+    {
+      type: "list",
+      message: "Who is the Employee's Manager?",
+      choices: [],
+      name: "empManager"
     },
 
   ])
 }
 
-// TODO: make a function to call when add a department is selected
+
 const updateEmployeeQ = () => {
   return inquirer.prompt([
     {
-
+      type: "list",
+      message: "Which Employee's role do you want to update?",
+      choices: [],
+      name: "employee"
+    },
+    {
+      type: "list",
+      message: "Which role do you want to assign the selected employee?",
+      choices: [],
+      name: "role"
     },
 
   ])
+
+  .then((answers) => {
+    db.query(``, function (err) {
+      if (err) throw err;
+      console.log("Updated employee's role!")
+    })
+  })
 }
 
 
